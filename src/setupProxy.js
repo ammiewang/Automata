00f5d042b92fb4a1e3a838b67710da3d3552e1ee
Ammie Wang <ammie.wang8@gmail.com>
2021-01-31 10:12:24 -0800

    separated out server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(createProxyMiddleware("/api", { target: "https://automata-app.herokuapp.com/" }));
+  app.use(createProxyMiddleware("/api",
+    { target: "https://automatapi.herokuapp.com/", changeOrigin: true }
+  ));
 };
